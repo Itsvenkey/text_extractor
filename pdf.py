1dd6536fdf715af70222d01e0ce907c6f724b437
Itsvenkey <137922952+Itsvenkey@users.noreply.github.com>
2024-03-29 14:22:01 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,11 +1,16 @@
 from pypdf import PdfReader
 
+#name of the file in the directory pdf
 name = 'sample'
+
+#pdf reader from the pypdf library to read the pdf file from the given directory
 reader = PdfReader(r'./pdf/'+name+'.pdf')
 
+#printing the number of pages
 print(len(reader.pages))
 
 
+#printing the data from the pdf file from its each page
 for i in range(len(reader.pages)):
     page = reader.pages[i]
     text = page.extract_text()
