38dbc96fd5e9a583b7d7ec7e8df82133075b84b3
Itsvenkey <137922952+Itsvenkey@users.noreply.github.com>
2024-03-29 14:27:37 +0530

    Update psd.py

--- psd.py
+++ psd.py
@@ -1,31 +1,37 @@
 import warnings
 from psd_tools import PSDImage
 
+
+#name of the psd file present in the directory psd
 name = 'diwali'
+
 # Open a PSD file
 psd = PSDImage.open(r'psd/'+name+'.psd')
-
-# Suppress all warnings globally
-warnings.filterwarnings("ignore")
 
 # Get the number of layers
 num_layers = len(psd._layers)
 
 layers = psd._layers
-
+# this loop is just to create a space between the warning and the actual output
 for i in range(10):
     print('')
 
+# Output the number of layers
+print("Number of layers:", num_layers)
+
+
+#this gives the resolutions of each layer
+for i in range(num_layers):
+    print(f'layer {i} has:\nwidth= {layers[i].size[0]},  height={layers[i].size[1]}')
+
+#this is to print the text from each layer
 for _layers in psd:
     if _layers.kind=='type':
         print(_layers.text)
 
 
 
-# Output the number of layers
-print("Number of layers:", num_layers)
 
 
-for i in range(num_layers):
-    print(f'layer {i} has:\nwidth= {layers[i].size[0]},  height={layers[i].size[1]}')
+
     
